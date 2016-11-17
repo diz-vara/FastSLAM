aa28c3115e0f1dd33e0c9889968d1498edacc0fa
diz_vara <avarfolomeev@navmii.com>
2016-11-17 17:44:30 +0300

    params

--- matlab/configfile.m
+++ matlab/configfile.m
@@ -16,19 +16,20 @@ Q= [sigmaV^2 0; 0 sigmaG^2];
 
 % observation parameters
 MAX_RANGE= 30.0; % metres
-DT_OBSERVE= 8*DT_CONTROLS; % seconds, time interval between observations
+DT_OBSERVE= 2*DT_CONTROLS; % seconds, time interval between observations
 
 % observation noises
-sigmaR= 0.2; % metres
-sigmaB= (2.0*pi/180); % radians
+sigmaR= 5.0; % metres
+sigmaB= (5.0*pi/180); % radians
 R= [sigmaR^2 0; 0 sigmaB^2];
 
 % waypoint proximity
 AT_WAYPOINT= 1.0; % metres, distance from current waypoint at which to switch to next waypoint
-NUMBER_LOOPS= 2; % number of loops through the waypoint list
+NUMBER_LOOPS= 3; % number of loops through the waypoint list
+
 
 % resampling
-NPARTICLES= 100; 
+NPARTICLES= 10; 
 NEFFECTIVE= 0.75*NPARTICLES; % minimum number of effective particles before resampling
 
 % switches
@@ -37,8 +38,8 @@ SWITCH_SENSOR_NOISE = 1;
 SWITCH_INFLATE_NOISE= 0;
 SWITCH_PREDICT_NOISE = 0;   % sample noise from predict (usually 1 for fastslam1.0 and 0 for fastslam2.0)
 SWITCH_SAMPLE_PROPOSAL = 1; % sample from proposal (no effect on fastslam1.0 and usually 1 for fastslam2.0)
-SWITCH_HEADING_KNOWN= 0;
+SWITCH_HEADING_KNOWN= 1;
 SWITCH_RESAMPLE= 1; 
-SWITCH_PROFILE= 1;
+SWITCH_PROFILE= 0;
 SWITCH_SEED_RANDOM= 0; % if not 0, seed the randn() with its value at beginning of simulation (for repeatability)
 
